44c0819f8902f93d488ba157b2b6190431d4cb79
aman086 <amancoding86@gmail.com>
2024-01-30 16:21:39 +0530

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftContainerComponent } from './left-container/left-container.component';
+import { RightContainerComponent } from './right-container/right-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftContainerComponent,
+    RightContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/right-container/right-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-container',
+  templateUrl: './right-container.component.html',
+  styleUrl: './right-container.component.css'
+})
+export class RightContainerComponent {
+
+}
